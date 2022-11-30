88bcaffea2f2a62e16dc98fab1b0d922b614243f
Mukesh Pasi <mukesh.pasi@apexon.com>
2022-11-30 09:53:55 +0530

    removed commented text

--- TestProject1/Lola.Hooks/DataSetup.cs
+++ TestProject1/Lola.Hooks/DataSetup.cs
@@ -12,24 +12,12 @@ namespace TestProject1.Lola.Hooks
     [Binding]
     public class DataSetup
     {
-        // to do config file- check local /db data en and write sql command query(ado.net)- to fetch test data from db
-        //FeatureContext featureContext;
-       // ScenarioContext scenarioContext;
-        // model class GET Set operation
-        //config file keys will be env, local,db(true/false), so we will pass constructor parameter of env and types of data(local/db)
+       
         [BeforeScenario]
         public void GetLocalData()
         {
-            //to do
-            // to do add test data(key, value) in context-hooks module
-            // fetch test data in step defintion by context.TryGetValue(key)
-            //this.scenarioContext = scenarioContext;
-
-
+            
         }
-        public void ParseJsonFile(string file)
-        {
-
-        }
+        
     }
 }
