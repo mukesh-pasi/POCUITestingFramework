0a3fbbd1014e9eee17feb3e952b3418799f13924
mukesh-pasi <119421596+mukesh-pasi@users.noreply.github.com>
2022-12-01 14:15:34 +0530

    Chnaged filename

--- TestProject/Lola.Features/Login.feature.cs
+++ TestProject/Lola.Features/Login.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace TestProject1.Lola_Features
+namespace TestProject.Lola_Features
 {
     using TechTalk.SpecFlow;
     using System;
