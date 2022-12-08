2efbfe706d3c9d813492fddf7375f551d6921f9f
mukesh-pasi <119421596+mukesh-pasi@users.noreply.github.com>
2022-12-08 12:40:10 +0530

    test commit

---
+++ TestProject/Lola.Hooks/TestConfig/TestConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace LolaUI.Data.TestConfig
+{
+    public class TestConfig
+    {
+        public string? username { get; set; }
+        public string? password { get; set; }
+        public string? env { get; set; }
+        public string? cache { get; set; }
+        public string? baseurl { get; set; }
+    }
+}
